eb4d79f9709c23eef296f6bf6ef0fbcd90fb3578
Nikhil Sharma <nikhilsharma9076@gmail.com>
2023-08-13 22:09:36 +0530

    Added more answers, upto 18 and 2 left in this

--- Question1/src/Main.java
+++ Question1/src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Scanner S = new Scanner(System.in);
+        System.out.println("Please enter a number : ");
+        int N = S.nextInt();
+
+        for(int i=0; i<N; i++) {
+            for (int star=0; star<N; star++)
+                System.out.print("*\t");
+            System.out.println();
+        }
     }
 }
--- Question2/src/Main.java
+++ Question2/src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner S = new Scanner(System.in);
+
+        System.out.println("Please enter a number : ");
+        int N = S.nextInt();
+
+        for(int i=0; i<N; i++)
+        {
+            for(int star=0; star<=i; star++)
+                System.out.print("*\t");
+            System.out.println();
+        }
     }
 }
